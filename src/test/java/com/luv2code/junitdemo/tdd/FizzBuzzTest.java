49637dd4728f8f65acb2c17ae415fc457cd872c8
JiangTaoHou <925147954@qq.com>
2023-01-12 15:11:31 +0800

    csv参数可以直接写在@CsvSource中去

--- src/test/java/com/luv2code/junitdemo/tdd/FizzBuzzTest.java
+++ src/test/java/com/luv2code/junitdemo/tdd/FizzBuzzTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
 import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -189,5 +191,18 @@ class FizzBuzzTest {
                     .map(Arguments::of);
         }
     }
+    Map<Long, String> idToUsername = new HashMap<>();
 
+    {
+        idToUsername.put(1L, "Selma");
+        idToUsername.put(2L, "Lisa");
+        idToUsername.put(3L, "Tim");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,Selma", "2,Lisa", "3,Tim"})
+    void testUsersFromCsv(long id, String name) {
+        assertTrue(idToUsername.containsKey(id));
+        assertTrue(idToUsername.get(id).equals(name));
+    }
 }
